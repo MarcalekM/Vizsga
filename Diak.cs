2be2b1ec378a3559cc638e55eeff75cb0892b2ef
Sufi <120452154+MarcalekM@users.noreply.github.com>
2025-01-20 10:26:59 +0100

    Add project files.

---
+++ Diak.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vizsga
+{
+    class Diak
+    {
+        public string Nev { get; set; }
+        public float HalozatIrasbeli { get; set; }
+        public float ProgramozasIrasbeli { get; set; }
+        public float HalozatA {  get; set; }
+        public float HalozatB { get; set; }
+        public float HalozatC { get; set; }
+        public float HalozatD { get; set; }
+        public float AngolSzobeli { get; set; }
+        public float ITSzobeli { get; set; }
+        public float Eredmeny => (HalozatIrasbeli + ProgramozasIrasbeli + HalozatA + HalozatB + HalozatC + HalozatD + AngolSzobeli + ITSzobeli) / 8;
+
+        public string Erdemjegy(float eredmeny)
+        {
+            if (eredmeny < 0.51) return "Elégtelen";
+            else if (eredmeny < 0.61) return "Elégséges";
+            else if (eredmeny < 0.71) return "Közepes";
+            else if (eredmeny < 0.81) return "Jó";
+            else return "Jeles";
+        }
+
+        public Diak(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            Nev = adatok[0];
+            HalozatIrasbeli = float.Parse(adatok[1]);
+            ProgramozasIrasbeli = float.Parse(adatok[2]);
+            HalozatA = float.Parse(adatok[3]);
+            HalozatB = float.Parse(adatok[4]);
+            HalozatC = float.Parse(adatok[5]);
+            HalozatD = float.Parse(adatok[6]);
+            AngolSzobeli = float.Parse(adatok[7]);
+            ITSzobeli = float.Parse(adatok[8]);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.Unicode;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Vizsga;
+
+namespace Vizsga
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Diak> diakok = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using StreamReader sr = new(
+                path: @"../../../src/adatok.txt",
+                encoding: Encoding.UTF8);
+
+            while (!sr.EndOfStream) diakok.Add(new(sr.ReadLine()));
+
+            VizsganResztvevokSzama.Content = $"{diakok.Count} diák vett részt a vizsgán";
+
+            foreach (var diak in diakok) VizsgazokNevei.Items.Add(diak.Nev);
+        }
+
+        private void SikeresVizsga_Click(object sender, RoutedEventArgs e)
+        {
+            int sikeresekSzama = 0;
+            foreach (var diak in diakok) if (diak.HalozatIrasbeli > 0.5 && diak.ProgramozasIrasbeli > 0.5 &&  diak.HalozatA > 0.5 && diak.HalozatB > 0.5 &&  diak.HalozatC > 0.5 &&  diak.HalozatD > 0.5 &&  diak.AngolSzobeli > 0.5 &&  diak.ITSzobeli > 0.5) sikeresekSzama++;
+            SikeresVizsgaSzoveg.Content = $"{sikeresekSzama} fő";
+        }
+
+        private void FajlbaIras_Click(object sender, RoutedEventArgs e)
+        {
+            using StreamWriter sw = new(
+                path: @"../../../src/vizsgaeredmenyek.txt",
+                append: false);
+            foreach (var diak in diakok) if (diak.Eredmeny > 0.5) sw.WriteLine($"{diak.Nev}\t{diak.Eredmeny}\t{diak.Erdemjegy(diak.Eredmeny)}");
+        }
+
+        private void KeresettTanulo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool NincsTalalat = true;
+            foreach (var diak in diakok)
+            {
+                if (diak.Nev.Contains(KeresettTanulo.Text))
+                {
+                    NincsTalalat = false;
+                }
+            }
+            if(NincsTalalat) MessageBox.Show("Nincs találat");
+        }
+    }
+}
